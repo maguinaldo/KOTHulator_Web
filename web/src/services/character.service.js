a1bd1b8be8f841bb1e1560453666f1ed1e2b8319
Mitch Aguinaldo <mitchellaguinaldo@gmail.com>
2018-03-01 17:37:55 -0800

    Comment cleanup

--- web/src/services/character.service.js
+++ web/src/services/character.service.js
@@ -49,25 +49,3 @@ const characterService = {
 };
 
 export default characterService;
-
-            // data: [
-            //     {
-            //         _id: 1,
-            //         name: "mitch",
-            //         type: "monSTAR",
-            //         imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Pentagram_green.svg/220px-Pentagram_green.svg.png"
-            //     },
-            //     {
-            //         _id: 2,
-            //         name: "rock",
-            //         type: "inanimate",
-            //         imageUrl: "https://geology.com/articles/difficult-rocks/difficult-rock-identification.jpg"
-
-            //     },
-            //     {
-            //         _id: 3,
-            //         name: "the TICK",
-            //         type: "bug",
-            //         imageUrl: "https://www.entertainmentearth.com/images/AUTOIMAGES/EL9000681lg.jpg"
-            //     }
-            // ]
